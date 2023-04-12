09c3ae9ed6ec8dfbd72f0cc9105a9b34f0eb8d5e
gib <gib@gibbyb.com>
2023-04-11 20:01:03 -0500

    Added PHP files for PHP server testing and configuration

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+$host = "172.17.0.8"; // Your database host (usually localhost)
+$db_name = "wired-world-db"; // The name of your database
+$username = "ww-db"; // Your MySQL username
+$password = "ww2023"; // Your MySQL password
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
+
+
---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+include 'config.php';
+
+if ($conn)
+{
+    echo "Successfully connected to the database!";
+} else
+{
+    echo "Failed to connect to the database!";
+}
+
+echo "I love Pizza!";
+echo "<br>Austin Rules!"
+
+?>
