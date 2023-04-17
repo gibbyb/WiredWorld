8619852673b81c7d34d692eec8e7788c6f2e1ea8
gib <gib@gib-server.gibbyb.com>
2023-04-17 02:18:06 -0500

    Added all product pages

---
+++ tablets.php
@@ -0,0 +1,49 @@
+<?php
+require_once 'config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Wired World - Products</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="img/wired-world-logo.png" type="image/png">
+</head>
+<body>
+<?php include 'header.php'; ?>
+<main id="products-main" style="margin-top: 100px;">
+    <section class="all-products">
+        <div class="product-container">
+            <?php
+            $sql = "SELECT * FROM products WHERE category_id = 2";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
+            $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            if (count($products) > 0) {
+                echo '<div class="product-row">';
+
+                $productCounter = 0;
+                foreach ($products as $product) {
+                    if ($productCounter % 4 == 0 && $productCounter > 0) {
+                        echo '</div><div class="product-row">';
+                    }
+                    echo '<div class="product-card">';
+                    echo '<img src="' . $product['thumbnail_path'] . '" alt="' . $product['name'] . '">';
+                    echo '<h3>' . $product['brand'] . ' ' . $product['name'] . '</h3>';
+                    echo '<p>' . $product['description'] . '</p>';
+                    echo '<button>Add to Cart</button>';
+                    echo '</div>';
+                    $productCounter++;
+                }
+
+                echo '</div>';
+            }
+            ?>
+        </div>
+    </section>
+</main>
+<?php include 'footer.php'; ?>
+</body>
+</html>
